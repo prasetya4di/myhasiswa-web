b844d987a3148e62ac2ceefe4e501b7d2401b2d1
praset <prasetyaarfanto@gmail.com>
2022-03-18 21:31:34 +0700

    add mahasiswa model, migration, and controller

---
+++ database/migrations/2022_03_18_141715_create_mahasiswas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string("nim")->index();
+            $table->string("nama");
+            $table->text("alamat");
+            $table->date("ttl");
+            $table->string("gender");
+            $table->string("prodi");
+            $table->string("no_handphone");
+            $table->string("email");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+};
