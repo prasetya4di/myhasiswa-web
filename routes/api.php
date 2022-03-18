cfe42b75b68d3d4ba73853967aa448ec8e19f12d
praset <prasetyaarfanto@gmail.com>
2022-03-18 22:26:29 +0700

    add api route for mahasiswa, matkul, tugas, and note

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\MataKuliahController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TugasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::controller(MahasiswaController::class)->group(function () {
+        Route::get("/mahasiswa", 'index');
+        Route::get("/mahasiswa/{nim}", 'show');
+        Route::post("/mahasiswa", 'store');
+        Route::put("/mahasiswa/{nim}", 'update');
+        Route::delete("/mahasiswa/{nim}", 'destroy');
+    });
+    Route::controller(MataKuliahController::class)->group(function () {
+        Route::get("/matkul", 'index');
+        Route::get("/matkul/{kode_matkul}", 'show');
+        Route::post("/matkul", 'store');
+        Route::put("/matkul/{kode_matkul}", 'update');
+        Route::delete("/matkul/{kode_matkul}", 'destroy');
+    });
+    Route::controller(TugasController::class)->group(function () {
+        Route::get("/tugas", 'index');
+        Route::get("/tugas/{nomor_tugas}", 'show');
+        Route::post("/tugas", 'store');
+        Route::put("/tugas/{nomor_tugas}", 'update');
+        Route::delete("/tugas/{nomor_tugas}", 'destroy');
+    });
+    Route::controller(NoteController::class)->group(function () {
+        Route::get("/note", 'index');
+        Route::get("/note/{nomor_note}", 'show');
+        Route::post("/note", 'store');
+        Route::put("/note/{nomor_note}", 'update');
+        Route::delete("/note/{nomor_note}", 'destroy');
+    });
 });
